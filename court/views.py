6d55047cdec80cbfa284b3a08e991d215323202b
Khaled El Hamzi <contact@elhamzi.me>
2021-07-17 02:48:47 +0200

    render dynamically

--- court/views.py
+++ court/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def startup(request):
-  return render(request, 'startup.html')
+    return render(request, 'startup.html', {'name': 'Khaled'})
